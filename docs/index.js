722d4961409d4e6444912a1ab3a3243acd5c128c
usjpin <65671230+usjpin@users.noreply.github.com>
2022-04-23 10:31:44 -0500

    make port dynamic

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const dotenv = require('dotenv')
+dotenv.config()
+
 const views = path.join(__dirname, 'views')
 app.use(express.static(views))
 
-const server = app.listen(8080)
+const PORT = process.env.PORT
+const server = app.listen(PORT)
